c0256e66f9a39f1c81870dc053ed5a17296c2063
feba6481 <feba.baumann@gmail.com>
2017-05-08 10:15:54 +0200

    hallllooo

--- src/beispielprojekt/Beispielprojekt.java
+++ src/beispielprojekt/Beispielprojekt.java
@@ -3,7 +3,11 @@ package beispielprojekt;
 public class Beispielprojekt {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("DEEERE");
     }
-    
+
+
+    public void mcBoumen(){
+        System.out.println("McBoumen");
+    }
 }
