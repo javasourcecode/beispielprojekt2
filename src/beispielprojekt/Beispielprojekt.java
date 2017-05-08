4e843144d656cfe40c233416397c037b0fb7517b
feba6481 <feba.baumann@gmail.com>
2017-05-08 10:25:00 +0200

    OCA

--- src/beispielprojekt/Beispielprojekt.java
+++ src/beispielprojekt/Beispielprojekt.java
@@ -2,7 +2,7 @@ package beispielprojekt;
 
 public class Beispielprojekt {
 
-    public static void main(String[] args) {
+    public static void main(Strin[] args) {
         System.out.println("DEEERE");
     }
 
